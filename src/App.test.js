a50dff37c4787d0e70035a1be04afd373308012b
Geraldin <geralpaez00@gmail.com>
2022-05-22 04:54:16 -0500

    Funcional_sin_errores

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,4 @@
 import { render, screen } from '@testing-library/react';
-<<<<<<< HEAD
-import App from './App';
-=======
-import App from './Components/App';
->>>>>>> 3ccc310d24b3575239bd0b1a8d30f6772562d538
 
 test('renders learn react link', () => {
   render(<App />);
